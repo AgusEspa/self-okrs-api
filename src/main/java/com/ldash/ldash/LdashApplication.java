ee02c44df9065b85941161a54140c356855422a2
Agustin Espana <agustinespania@gmail.com>
2021-11-13 10:16:16 -0300

    Initial commit

---
+++ src/main/java/com/ldash/ldash/LdashApplication.java
@@ -0,0 +1,13 @@
+package com.ldash.ldash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ldash/ldash/LdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldash.ldash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
