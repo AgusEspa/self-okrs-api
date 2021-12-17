a2e8bc462103d60e27778934620be79ed6b56d98
Agustin Espana <agustinespania@gmail.com>
2021-12-17 19:54:36 -0300

    fixed exception handling

---
+++ src/main/java/me/projects/SelfOKRs/exceptions/ExceptionsControllerAdvice.java
@@ -0,0 +1,28 @@
+package me.projects.SelfOKRs.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionsControllerAdvice {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String userNotFoundHandler(UserNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String taskNotFoundHandler(TaskNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(GoalNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String goalNotFoundHandler(GoalNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
