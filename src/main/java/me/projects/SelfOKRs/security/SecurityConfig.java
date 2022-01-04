39c68eab4418dfcab61d7fa7b3dbee19b9f9f1a0
Agustin Espana <agustinespania@gmail.com>
2022-01-04 11:21:04 -0300

    Update SecurityConfig.java

--- src/main/java/me/projects/SelfOKRs/security/SecurityConfig.java
+++ src/main/java/me/projects/SelfOKRs/security/SecurityConfig.java
@@ -5,19 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
 
+    private final PasswordEncoder pwEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
     @Bean
-    UserDetailsService authentication() {
+    UserDetails authentication() {
         UserDetails agus = User.builder()
                 .username("agus")
-                .password("p")
+                .password(pwEncoder.encode("p"))
                 .roles("ADMIn")
                 .build();
 
-        return new InMemoryUserDetailsManager(agus);
+        return agus;
     }
 }
