eb0c6da46b625e4e33c90166454b520906372b3e
Agustin Espana <agustinespania@gmail.com>
2022-01-04 09:49:26 -0300

    Create TestUserService.java

---
+++ src/test/java/me/projects/SelfOKRs/services/TestUserService.java
@@ -0,0 +1,32 @@
+package me.projects.SelfOKRs.services;
+
+import me.projects.SelfOKRs.entities.User;
+import me.projects.SelfOKRs.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class TestUserService {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    public void shouldCreateNewUser() {
+        User newUser = new User("test1", "test1@mail.com", "testing_pass1");
+
+        when(userRepository.save(newUser)).thenReturn(newUser);
+
+        User created = userService.newUser(newUser);
+
+        assertEquals(created.getUsername(), newUser.getUsername());
+    }
+}
