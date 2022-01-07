fbbe0e1c97ea5b5b50c130625da608c0363cfcfb
Agustin Espana <agustinespania@gmail.com>
2022-01-07 19:33:09 -0300

    Update SecurityConfig.java

--- src/main/java/me/projects/SelfOKRs/security/SecurityConfig.java
+++ src/main/java/me/projects/SelfOKRs/security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.httpBasic();
         http.authorizeRequests()
                 .anyRequest().authenticated();
