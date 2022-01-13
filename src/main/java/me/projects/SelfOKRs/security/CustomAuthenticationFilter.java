10d07e22ebcb96b372514369043411a3079eb318
Agustin Espana <agustinespania@gmail.com>
2022-01-13 11:28:28 -0300

    Create CustomAuthenticationFilter.java

---
+++ src/main/java/me/projects/SelfOKRs/security/CustomAuthenticationFilter.java
@@ -0,0 +1,31 @@
+package me.projects.SelfOKRs.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public CustomAuthenticationFilter(CustomAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationProvider.authenticate(authenticationToken);
+    }
+}
