9c9afda5966fa1bbfee323f27d2e52de83bebea6
Agustin Espana <agustinespania@gmail.com>
2022-01-14 13:18:34 -0300

    Update CustomAuthorizationFilter.java

--- src/main/java/me/projects/SelfOKRs/security/CustomAuthorizationFilter.java
+++ src/main/java/me/projects/SelfOKRs/security/CustomAuthorizationFilter.java
@@ -27,6 +27,7 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
