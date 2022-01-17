7485dd5f9fda43f5c63011a8ff14548eb05f5c6e
Agustin Espana <agustinespania@gmail.com>
2022-01-17 18:44:27 -0300

    Create AuthenticationFacade.java

---
+++ src/main/java/me/projects/SelfOKRs/security/AuthenticationFacade.java
@@ -0,0 +1,13 @@
+package me.projects.SelfOKRs.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationFacade {
+
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
