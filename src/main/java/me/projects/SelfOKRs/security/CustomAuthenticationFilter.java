3a4914be93e8d0bf51c43be4e42f2ea93a57e90c
Agustin Espana <agustinespania@gmail.com>
2022-02-03 17:28:05 -0300

    fixes

--- src/main/java/me/projects/SelfOKRs/security/CustomAuthenticationFilter.java
+++ src/main/java/me/projects/SelfOKRs/security/CustomAuthenticationFilter.java
@@ -51,7 +51,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
         String access_token = JWT.create()
                 .withSubject(authenticatedUser.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 1 * 60 * 1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles", authenticatedUser.getAuthorities().stream()
                         .map(GrantedAuthority::getAuthority)
--- src/main/java/me/projects/SelfOKRs/security/TokenService.java
+++ src/main/java/me/projects/SelfOKRs/security/TokenService.java
@@ -46,7 +46,7 @@ public class TokenService {
                 UserDetails user = userDetailsService.loadUserByUsername(username);
                 String access_token = JWT.create()
                         .withSubject(user.getUsername())
-                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 1000))
                         .withIssuer(request.getRequestURL().toString())
                         .withClaim("roles", user.getAuthorities().stream()
                                 .map(GrantedAuthority::getAuthority)
@@ -54,7 +54,6 @@ public class TokenService {
                         .sign(algorithm);
                 Map<String, String> tokens = new HashMap<>();
                 tokens.put("access_token", access_token);
-                tokens.put("refresh_token", refresh_token);
                 response.setContentType(MediaType.APPLICATION_JSON_VALUE);
                 new ObjectMapper().writeValue(response.getOutputStream(), tokens);
             } catch (Exception exception) {
